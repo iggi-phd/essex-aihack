f238f3e60f9a90545d2e4b7da26c086e6d35830d
davidgundry <davidgundry@gmail.com>
2015-06-12 09:43:03 +0100

    DaveControllers fighting each other

--- src/battle/BattleTest.java
+++ src/battle/BattleTest.java
@@ -18,7 +18,7 @@ public class BattleTest {
         SimpleBattle battle = new SimpleBattle();
 
         BattleController player1 = new DaveController();
-        BattleController player2 = new MemoControllerRandom();
+        BattleController player2 = new DaveController();
         battle.playGame(player1, player2);
     }
 
